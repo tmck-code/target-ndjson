4202d19ba477c112584b2bb4c0c08f51ec093d94
Tom McKeesick <tmck01@gmail.com>
2020-07-29 21:37:37 +1000

    Move processing code into separate module
    - Save __init__ just for the arg parsing and singer requests
    - Normalise quote style

--- setup.py
+++ setup.py
@@ -2,21 +2,22 @@
 from setuptools import setup
 
 setup(
-    name="target-ndjson",
-    version="0.1.0",
-    description="Singer.io target for extracting data",
-    author="Stitch",
-    url="http://singer.io",
-    classifiers=["Programming Language :: Python :: 3 :: Only"],
-    py_modules=["target_ndjson"],
+    name='target-ndjson',
+    version='0.1.0',
+    description='Singer.io target for extracting data to ndjson files',
+    author='Stitch',
+    url='http://singer.io',
+    classifiers=['Programming Language :: Python :: 3 :: Only'],
+    py_modules=['target_ndjson'],
     install_requires=[
-        "singer-python>=5.0.12",
+        'singer-python>=5.0.12',
+        'ujson'
     ],
-    entry_points="""
+    entry_points='''
     [console_scripts]
     target-ndjson=target_ndjson:main
-    """,
-    packages=["target_ndjson"],
-    package_data = {},
+    ''',
+    packages=['target_ndjson'],
+    package_data={},
     include_package_data=True,
 )
