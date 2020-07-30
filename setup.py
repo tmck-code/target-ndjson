e7651fff37cc805e048df8645cce16df49166e84
Tom McKeesick <tmck01@gmail.com>
2020-07-30 22:44:04 +1000

    Increment the version number

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup
 
 setup(
     name='target-ndjson',
-    version='0.1.0',
+    version='0.1.1',
     description='Singer.io target for extracting data to ndjson files',
     author='Stitch',
     url='http://singer.io',
