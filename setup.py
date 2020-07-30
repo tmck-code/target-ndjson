975eae47be9864884d2f6ef6f814de4a551c4bc0
Tom McKeesick <tmck01@gmail.com>
2020-07-30 22:27:15 +1000

    Add pylintrc file and fix all violations

--- setup.py
+++ setup.py
@@ -1,4 +1,6 @@
 #!/usr/bin/env python
+'Defines metadata for target-ndjson'
+
 from setuptools import setup
 
 setup(
--- target_ndjson/target.py
+++ target_ndjson/target.py
@@ -4,38 +4,32 @@ non-transformative as possible during the process
 '''
 
 from datetime import datetime
-import collections
 import sys
-import ujson as json
+import ujson
 
 from jsonschema.validators import Draft4Validator
+
+class InvalidTapJSON(ValueError):
+    'Raised whenever a Tap output log cannot be parsed'
 
 def emit_state(state, logger):
     'Emit state via logger and STDOUT'
     if state is not None:
-        line = json.dumps(state)
+        line = ujson.dumps(state)
         logger.debug(f'Emitting state {line}')
         sys.stdout.write(f'{line}\n')
         sys.stdout.flush()
 
-def flatten(d, parent_key='', sep='__'):
-    items = []
-    for k, v in d.items():
-        new_key = parent_key + sep + k if parent_key else k
-        if isinstance(v, collections.MutableMapping):
-            items.extend(flatten(v, new_key, sep=sep).items())
-        else:
-            items.append((new_key, str(v) if isinstance(v, list) else v))
-    return dict(items)
-
 def __fpath_for_stream(stream_name, timestamp):
     return '_'.join([stream_name, timestamp])
 
-def persist_lines(config, lines, logger):
+def persist_lines(_config, lines, logger):
+    'Triages and writes the logs lines to physical files on disk'
+
     state = None
     schemas = {}
     key_properties = {}
-    headers = {}
+    _headers = {}
     validators = {}
 
     now = datetime.now().strftime('%Y%m%dT%H%M%S')
@@ -43,46 +37,46 @@ def persist_lines(config, lines, logger):
     # Loop over lines from stdin
     for line in lines:
         try:
-            o = json.loads(line)
-            assert isinstance(o, dict)
-        except (ValueError, AssertionError):
+            obj = ujson.loads(line)
+            assert isinstance(obj, dict)
+        except (ValueError, AssertionError) as e:
             logger.error(f'Unable to parse:\n{line}')
-            raise
+            raise InvalidTapJSON(f'Unable to parse: {e} - {line}')
 
-        if 'type' not in o:
+        if 'type' not in obj:
             raise Exception(f'Line is missing required key "type": {line}')
 
-        if o['type'] == 'RECORD':
-            if 'stream' not in o:
+        if obj['type'] == 'RECORD':
+            if 'stream' not in obj:
                 raise Exception(f'Line is missing required key "stream": {line}')
-            if o['stream'] not in schemas:
-                raise Exception(f'A record for stream {o["stream"]} was encountered before a corresponding schema')
+            if obj['stream'] not in schemas:
+                raise Exception(
+                    f'A record for stream {obj["stream"]} was encountered before a '
+                    'corresponding schema'
+                )
 
             # Get schema for this record's stream
-            # schema = schemas[o['stream']]
+            # schema = schemas[obj['stream']]
             # Validate record
-            validators[o['stream']].validate(o['record'])
+            validators[obj['stream']].validate(obj['record'])
             # Write to file
-            with open(__fpath_for_stream(o['stream'], now), 'a') as ostream:
-                ostream.write(json.dumps(o['record']) + '\n')
+            with open(__fpath_for_stream(obj['stream'], now), 'a') as ostream:
+                ostream.write(ujson.dumps(obj['record']) + '\n')
 
             state = None
-        elif o['type'] == 'STATE':
-            logger.debug('Setting state to {o["value"]}')
-            state = o['value']
-        elif o['type'] == 'SCHEMA':
-            if 'stream' not in o:
+        elif obj['type'] == 'STATE':
+            logger.debug('Setting state to {obj["value"]}')
+            state = obj['value']
+        elif obj['type'] == 'SCHEMA':
+            if 'stream' not in obj:
                 raise Exception(f'Line is missing required key "stream": {line}')
-            stream = o['stream']
-            schemas[stream] = o['schema']
-            validators[stream] = Draft4Validator(o['schema'])
-            if 'key_properties' not in o:
+            stream = obj['stream']
+            schemas[stream] = obj['schema']
+            validators[stream] = Draft4Validator(obj['schema'])
+            if 'key_properties' not in obj:
                 raise Exception('key_properties field is required')
-            key_properties[stream] = o['key_properties']
+            key_properties[stream] = obj['key_properties']
         else:
-            raise Exception(f'Unknown message type {o["type"]} in message {o}')
+            raise Exception(f'Unknown message type {obj["type"]} in message {obj}')
 
     return state
-
-
-
