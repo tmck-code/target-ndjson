44608ae1bebd169ba186d4c0c2d9ffb759a1e2e5
Tom McKeesick <tmck01@gmail.com>
2020-08-12 10:44:20 +1000

    Test contents of output file
    Add a teardown method

--- test/test_target.py
+++ test/test_target.py
@@ -1,63 +1,64 @@
 'Test the target-ndjson module'
 
+import glob
+import os
+
 import singer
 import ujson
 
 from target_ndjson import target
 
+def state_log(updated_at='2020-07-05T00:00:00.000000Z'):
+    return ujson.dumps({
+        'type': 'STATE',
+        'value': {
+            'bookmarks': {'currently_sync_stream': 'orders', 'orders': {'updated_at': updated_at}}
+        }
+    })
+
+def schema_log(properties: dict, key_properties: list):
+    return ujson.dumps({
+        'type': 'SCHEMA',
+        'stream': 'orders',
+        'schema': {'properties': properties},
+        'key_properties': key_properties
+    })
+
+def record_log(record: dict):
+    return ujson.dumps({
+        'type': 'RECORD',
+        'stream': 'orders',
+        'record': record
+    })
 
 class TestTargetNDJSON:
     'Tests the target-ndjson module'
 
-    def setup_class(self):
-        self.schema_log = {
-            'type': 'SCHEMA',
-            'stream': 'orders',
-            'schema': {
-                'properties': {
-                    'transaction_amount': {'type': ['null', 'string']},
-                    'created_at': {'type': 'string'},
-                    'email': {'type': 'string'}
-                }
-            },
-            'key_properties': ['email']
-        }
-        self.state_logs = [
-            {
-                'type': 'STATE',
-                'value': {
-                    'bookmarks': {'currently_sync_stream': 'orders', 'orders': {'updated_at': '2020-07-05T00:00:00.000000Z'}}
-                }
-            },
-            {
-                'type': 'STATE',
-                'value': {
-                    'bookmarks': {'currently_sync_stream': 'orders', 'orders': {'updated_at': '2020-07-30T00:00:00.000000Z'}}
-                }
-            },
-            {
-                'type': 'STATE',
-                'value': {
-                    'bookmarks': {'currently_sync_stream': 'orders', 'orders': {'updated_at': '2020-08-05T00:00:00.000000Z'}}
-                }
-            }
-        ]
-        self.record_log = {
-            'type': 'RECORD',
-            'stream': 'orders',
-            'record': {
-                'email': 'customer@test.com',
-                'created_at': '2020-07-30T01:50:06.000000Z',
-                'transaction_amount': '12.23'
-            }
-        }
+    def teardown_method(self):
+        for fpath in glob.glob('orders_*.ndjson'):
+            os.remove(fpath)
 
     def test_parse(self):
         config = {
-            "start_date": "2020-01-01",
-            "api_key": "XXX",
-            "shop": "myshop"
+            'start_date': '2020-01-01',
+            'api_key': 'XXX',
+            'shop': 'myshop'
         }
-        json_log_lines = [ujson.dumps(l) for l in [self.schema_log, *self.state_logs, self.record_log]]
+        order_schema = schema_log(
+            {
+                'transaction_amount': {'type': ['null', 'string']},
+                'created_at': {'type': 'string'},
+                'email': {'type': 'string'}
+            },
+            ['email']
+        )
+        order_record = record_log({'transaction_amount': '12.34', 'created_at': '2020-01-01T00:00:00.000000Z', 'email': 'test@mail.com'})
+        expected = '''{"transaction_amount":"12.34","created_at":"2020-01-01T00:00:00.000000Z","email":"test@mail.com"}\n'''
 
-        target.TargetNDJSON(config, singer.get_logger()).persist_lines(json_log_lines)
+        obj = target.TargetNDJSON(config, singer.get_logger())
+        obj.persist_lines([order_schema, state_log(), order_record])
+
+        fpaths = glob.glob('orders_*.ndjson')
+        assert len(fpaths) == 1
+        with open(fpaths[0], 'r') as istream:
+            assert expected == istream.read()
